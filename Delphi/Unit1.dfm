ff8ae06f4094f724315ee5ba71fec1844d5986ef
Melissa Gobatti <53240154+melissagobatti@users.noreply.github.com>
2024-04-06 11:44:03 -0300

    Add files via upload

---
+++ Delphi/Unit1.dfm
@@ -0,0 +1,90 @@
+﻿object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'WhatsApp - Whaticket'
+  ClientHeight = 198
+  ClientWidth = 491
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Número: TLabel
+    Left = 16
+    Top = 8
+    Width = 37
+    Height = 13
+    Caption = 'N'#250'mero'
+  end
+  object Mensagem: TLabel
+    Left = 16
+    Top = 50
+    Width = 51
+    Height = 13
+    Caption = 'Mensagem'
+  end
+  object Label1: TLabel
+    Left = 224
+    Top = 8
+    Width = 80
+    Height = 13
+    Caption = 'Caminho arquivo'
+  end
+  object edtNumber: TEdit
+    Left = 16
+    Top = 24
+    Width = 185
+    Height = 21
+    NumbersOnly = True
+    TabOrder = 0
+  end
+  object memoMessage: TMemo
+    Left = 16
+    Top = 64
+    Width = 185
+    Height = 89
+    TabOrder = 1
+  end
+  object btEnviaMSG: TButton
+    Left = 16
+    Top = 159
+    Width = 185
+    Height = 25
+    Caption = 'Enviar MSG'
+    TabOrder = 2
+    OnClick = btEnviaMSGClick
+  end
+  object edtFile: TEdit
+    Left = 224
+    Top = 24
+    Width = 121
+    Height = 21
+    TabOrder = 3
+  end
+  object btnFile: TButton
+    Left = 351
+    Top = 22
+    Width = 121
+    Height = 25
+    Caption = 'File'
+    TabOrder = 4
+    OnClick = btnFileClick
+  end
+  object btnEnviarFile: TButton
+    Left = 296
+    Top = 53
+    Width = 121
+    Height = 25
+    Caption = 'Enviar File'
+    TabOrder = 5
+    OnClick = btnEnviarFileClick
+  end
+  object OpenDialog: TOpenDialog
+    Left = 440
+    Top = 56
+  end
+end
---
+++ Delphi/Unit1.pas
@@ -0,0 +1,113 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtDlgs, IdHTTP, IdMultipartFormData,
+  IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient, IdSSLOpenSSL,
+  IdAntiFreezeBase, IdAntiFreeze;
+
+Const URL = 'https://api.siwhaticket.com.br/api/messages/send';
+Const Token = 'f7d2f4c2-d7d5-40b0-bbae-d5727bf6af00';
+
+type
+  TForm1 = class(TForm)
+    edtNumber: TEdit;
+    memoMessage: TMemo;
+    Número: TLabel;
+    Mensagem: TLabel;
+    btEnviaMSG: TButton;
+    edtFile: TEdit;
+    btnFile: TButton;
+    OpenDialog: TOpenDialog;
+    btnEnviarFile: TButton;
+    Label1: TLabel;
+    procedure btEnviaMSGClick(Sender: TObject);
+    procedure btnFileClick(Sender: TObject);
+    procedure btnEnviarFileClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btEnviaMSGClick(Sender: TObject);
+var
+ IdHTTP: TIdHTTP;
+ IdSSL:  TIdSSLIOHandlerSocketOpenSSL;
+ RequestBody: TStringStream;
+begin
+
+  IdHTTP      := TIdHTTP.Create;
+  IdSSL       := TIdSSLIOHandlerSocketOpenSSL.Create(IdHTTP);
+  RequestBody := TStringStream.Create(UTF8String('{ "number": "55'+edtNumber.Text+'", "body": "'+memoMessage.Text+'" }'));
+
+  try
+    IdSSL.SSLOptions.Mode   := sslmClient;
+    IdSSL.SSLOptions.Method := sslvTLSv1_2;
+    IdHTTP.IOHandler        := IdSSL;
+
+    IdHTTP.HTTPOptions     := IdHTTP.HTTPOptions + [hoForceEncodeParams];
+    IdHTTP.HandleRedirects := True;
+
+    IdHTTP.Request.CustomHeaders.Clear;
+    IdHTTP.Request.UserAgent   := 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0';
+    IdHTTP.Request.CharSet     := 'utf-8';
+    IdHTTP.Request.ContentType := 'application/json;';
+    IdHTTP.Request.CustomHeaders.Add('Authorization: Bearer '+Token);
+    IdHTTP.Post(URL, RequestBody);
+
+    ShowMessage('Mensagem enviada com sucesso');
+
+  finally
+    IdHTTP.Free;
+    RequestBody.Free;
+  end;
+end;
+
+procedure TForm1.btnEnviarFileClick(Sender: TObject);
+var
+ IdHTTP: TIdHTTP;
+ IdSSL:  TIdSSLIOHandlerSocketOpenSSL;
+ Params: TIdMultiPartFormDataStream;
+ Response: string;
+begin
+  IdHTTP   := TIdHTTP.Create;
+  IdSSL    := TIdSSLIOHandlerSocketOpenSSL.Create(IdHTTP);
+  Params   := TIdMultiPartFormDataStream.Create;
+  try
+    IdSSL.SSLOptions.Mode   := sslmClient;
+    IdSSL.SSLOptions.Method := sslvTLSv1_2;
+    IdHTTP.IOHandler        := IdSSL;
+
+    IdHTTP.HTTPOptions     := IdHTTP.HTTPOptions + [hoForceEncodeParams];
+    IdHTTP.HandleRedirects := True;
+
+    Params.AddFormField('number', '55'+edtNumber.Text);
+    Params.AddFile('medias', edtFile.Text);
+    IdHTTP.Request.CustomHeaders.Add('Authorization: Bearer '+Token);
+    Response := IdHTTP.Post(URL, Params);
+
+    ShowMessage('Arquivo enviado com sucesso');
+  finally
+    Params.Free;
+    IdHTTP.Free;
+    IdSSL.Free;
+  end;
+end;
+
+procedure TForm1.btnFileClick(Sender: TObject);
+begin
+ if OpenDialog.Execute then
+  edtFile.Text := OpenDialog.FileName;
+end;
+
+end.
